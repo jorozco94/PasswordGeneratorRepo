5e4fe7d3575376a6215948394457d58899ee4c46
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2022-11-07 20:10:17 -0800

    Renaming css and js files and updating references in HTML

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Array containing the characters we'll use to generate our passwords
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+
+// Grabbing the HTML collection of our elements with classname pass
+const passContainers = document.getElementsByClassName("pass");
+// Create array from our HTML collection resulting from above fxn
+const passContainersArray = Array.from(passContainers);
+
+
+// Fxn runs thru char array and randomly pushes 15 chars into new arr and joins them into a password string
+function generate() {
+    const passwordArr = [];
+    for (let i = 0; i < 15; i++) {
+        passwordArr.push(characters[Math.floor(Math.random() * characters.length)]);
+    };
+    return passwordArr.join("");
+}
+
+// Appends randomly generated password from generate() to HTML element
+function appendPW() {
+    passContainersArray.map((container) => container.textContent = generate())
+}
+
