320e2ec11e749468c50259cd1f40ae72ffcd3a62
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2022-11-07 20:04:38 -0800

    Publishing code for PW generator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Array containing the characters we'll use to generate our passwords
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
 
+// Grabbing the HTML collection of our elements with classname pass
 const passContainers = document.getElementsByClassName("pass");
 // Create array from our HTML collection resulting from above fxn
 const passContainersArray = Array.from(passContainers);
